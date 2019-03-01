c153f3f520f827033e954165dbe2d2205840e8b6
Gisela Torres Buitrago <giselatb@outlook.com>
2019-03-01 08:51:38 +0000

    first commit

---
+++ WebAppWithAzureAD/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAppWithAzureAD.Models;
+
+namespace WebAppWithAzureAD.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //var claims = HttpContext.User.Claims;
+
+            //var rolesClaim = HttpContext.User.Claims.Where(c => c.Type == ClaimsIdentity.DefaultNameClaimType).FirstOrDefault();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppWithAzureAD/Controllers/TestRoleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppWithAzureAD.Controllers
+{
+    public class TestRoleController : Controller
+    {
+        [Authorize(Policy = "Admins")]
+        public IActionResult Index()
+        {
+            ViewBag.WhoYouAre = "You are an admin";
+            return View("Index");
+        }
+
+        [Authorize(Policy = "Test")]
+        public IActionResult Test()
+        {
+            ViewBag.WhoYouAre = "You are a test";
+            return View("Index");
+        }
+
+        [Authorize(Policy = "CrossZone")]
+        public IActionResult CrossZone()
+        {
+            ViewBag.WhoYouAre = "You are in CrossZone";
+            return View("Index");
+        }
+    }
+}
